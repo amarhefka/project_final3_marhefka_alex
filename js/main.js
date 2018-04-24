b058e0e3fc2dba47149a0c4b16cfa94c3b51dd6c
Alex <alex.marhefka@gmail.com>
2018-04-23 22:55:00 -0400

    Add custom JavaScript

--- js/main.js
+++ js/main.js
@@ -22,4 +22,19 @@ $(document).ready(function () {
     });
 
     $.backstretch('img/tiger-pattern2.jpg');
+
+    // this is plain custom JavaScript to add the days of the week (and align them with the 'day' CSS class) before every other <time> element on the About page
+    var hours = document.getElementsByTagName('time');
+    var days = ['MON: ', 'TUE: ', 'WED: ', 'THU: ', 'FRI: ', 'SAT: ', 'SUN: '];
+    var dayIndex = 0;
+    for (var i = 0; i < hours.length; i++) {
+        if (i % 2 === 0) {
+            var daySpan = document.createElement('span');
+            daySpan.setAttribute('class', 'day');
+            var dayNode = document.createTextNode(days[dayIndex]);
+            daySpan.appendChild(dayNode);
+            document.getElementById('hours').insertBefore(daySpan, hours[i]);
+            dayIndex += 1;
+        }
+    }
 });
